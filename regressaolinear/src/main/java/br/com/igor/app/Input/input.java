e5eb77ecd89f96cefbd8afc2c0a05663542c2c83
Igor Alves <igor193@live.com>
2023-09-01 05:46:29 -0300

    input

--- regressaolinear/src/main/java/br/com/igor/app/Input/input.java
+++ regressaolinear/src/main/java/br/com/igor/app/Input/input.java
@@ -9,21 +9,23 @@ import br.com.igor.app.Objetos.x;
 
 public class input {
 
-    private boolean condition = false;
 
     private List<Double> listX = new ArrayList<>();
+
+    private List<Double> listY = new ArrayList<>();
 
     private Scanner sc = new Scanner(System.in);
 
     public void atomicCreate() {
         addValuesInListX();
+        addValuesInListY();
         showListx();
         sc.close();
     }
 
     public void addValuesInListX() {
         x valueOfX = new x("");
-        System.out.print("Digite o valor de (X) ou 'proximo' para digitar o proximo parametro ou encerrar: ");
+        System.out.print("Digite o valor de (X) ou 'proximo' para digitar o proximo parametro: ");
         while (!valueOfX.atribuirValor(sc.nextLine(), 'X')) {
             if (valueOfX.getDoubleValue() != null) {
                 listX.add(valueOfX.getDoubleValue());
@@ -32,9 +34,22 @@ public class input {
         }
     }
 
+    public void addValuesInListY() {
+        fx valueOfX = new fx("");
+        System.out.print("Digite o valor de (Y) ou 'proximo' para encerrar: ");
+        while (!valueOfX.atribuirValor(sc.nextLine(), 'Y')) {
+            if (valueOfX.getDoubleValue() != null) {
+                listY.add(valueOfX.getDoubleValue());
+            }
+            valueOfX = new fx("");
+        }
+    }
+
     public void showListx() {
+        int i = 0;
         for (Double x : listX) {
-            System.out.println(x);
+            System.out.println(x + "  " + listY.get(i));
+            i++;
         }
     }
 }
